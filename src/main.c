a35087cb89aab6bfc18fb1d2a1013fc7f0334f7a
Andrew Benson <abenson@gmail.com>
2012-07-30 19:53:33 -0500

    Add standard arguments handling.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,31 @@
-#include <windows.h>
-#include <tchar.h>
+#include "common.h"
 
-int _tmain(int argc, TCHAR *argv)
+#include "arguments.h"
+
+TCHAR *get_version(void)
 {
+	return _T("HostKit Agent PREALPHA\nCompiled at ") __TIME__ _T(" on ") __DATE__ _T("\n");
+}
+
+int _tmain(int argc, TCHAR *argv[])
+{
+	initialize_arguments();
+
+	if(parse_arguments(argv) != ARGS_OK || arguments.error) {
+		_tprintf(_T("%s"), get_help());
+		quit();
+	}
+
+	if(arguments.debug) {
+		printf("Debug log is enabled.\n");
+	}
+
+	if(arguments.version == TRUE) {
+		_tprintf(_T("%s"), get_version());
+		quit();
+	}
+
+	
+
 	return 0;
 }
