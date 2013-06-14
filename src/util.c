29b98db6665b4af0e8a83cd23733f955b0752d2b
Jacob Hammack <jacob.hammack@hammackj.com>
2013-06-13 21:28:11 -0500

    Moved to common lib

--- src/util.c
+++ src/util.c
@@ -2,17 +2,6 @@
 
 #include "util.h"
 
-_TCHAR *dupestr(const _TCHAR *str)
-{
-	_TCHAR *duped;
-	duped = malloc(sizeof(*duped) * (_tcslen(str)+1));
-	if(duped) {
-		_tcsncpy(duped, str, _tcslen(str));
-	}
-	return NULL;
-}
 
-void quit(void)
-{
-	exit(EXIT_SUCCESS);
-}
+
+
--- src/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-_TCHAR *dupestr(const _TCHAR *str);
-
-void quit(void);
-
-#endif
