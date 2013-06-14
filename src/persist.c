05c2532423885536c531f80d6f557e4d81534265
Jacob Hammack <jacob.hammack@hammackj.com>
2013-06-13 23:53:53 -0500

    Include tweaks

--- src/persist.c
+++ src/persist.c
@@ -1,9 +1,13 @@
-#include "common.h"
+#include <Windows.h>
+#include <tchar.h>
+
+#include <stdio.h>
 
 #include "persist.h"
 
 int persist(void)
 {
 	_ftprintf(stderr, _T("Persistent mode not implemented!\n"));
+	//_ftprintf_s(
 	return -1;
 }
