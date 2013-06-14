583e54d04f4298c2990af3ff78cc137dfc43e42f
Jacob Hammack <jacob.hammack@hammackj.com>
2013-06-13 23:55:17 -0500

    Added hostname support
    Logging is commented out atm, it doesn't work... abenson

--- src/modules/basic/host_details.c
+++ src/modules/basic/host_details.c
@@ -1,25 +1,78 @@
-#include "../../common.h"
+#include "modules\modules.h"
+
+#define _WIN32_WINNT 0x0500
+
+static log_t *log;
+
+static int hostname(void);
+static int os_info(void);
 
 int host_details(void)
 {
+	//log = open_log(_T("-"), _T("json"));
 
-	return 0;
+	//if (log == NULL)
+	//{
+	//	return ERR_MODFAIL;
+	//}
+	
+	//open_section(log, _T("host_details"));
+
+	hostname();
+	os_info();
+
+	//close_section(log);
+	//close_log(log);
+	
+	return ERR_NONE;
 }
 
+//Based on
+//http://msdn.microsoft.com/en-us/library/windows/desktop/ms724301%28v=vs.85%29.aspx
 static int hostname(void)
 {
-	_TCHAR *name = NULL;
-	DWORD nameLen = 0;
+	TCHAR buffer[256] = _T("");
+	TCHAR fields[8][32] = 
+	{
+		_T("netbios"),
+		_T("dns_hostname"),
+		_T("dns_domain"),
+		_T("dns_fqdn"),
+		_T("pyhsical_netbios"),
+		_T("physical_dns_hostname"),
+		_T("physical_dns_domain"),
+		_T("physical_dns_fqdn")
+	};
 
-	/* Get size of buffer. */
-	GetComputerNameExW(ComputerNameDnsHostname, name, &nameLen);
-	
-	/* Get name of host. */
-	name = malloc(sizeof(*name) * nameLen);
-	GetComputerNameExW(ComputerNameDnsHostname, name, &nameLen);
+	DWORD length = sizeof(buffer);
+	int i = 0;
+		
+	for(i = 0; i < ComputerNameMax; i++)
+	{
+		if (!GetComputerNameEx((COMPUTER_NAME_FORMAT) i, buffer, &length))
+		{
+			return ERR_MODFAIL;
+		}
+		else
+		{
+			_tprintf_s(_T("Field: %s : %s\n"), fields[i], buffer);
+			//open_item(log);
+			//add_value(log, fields[i], buffer);
+			//close_item(log);
+		}
 
-	
+		length = _countof(buffer);
+		ZeroMemory(buffer, length);
+	}
 
-	free(name);
+	return ERR_NONE;
 }
 
+//Based on
+//http://msdn.microsoft.com/en-us/library/windows/desktop/ms724429%28v=vs.85%29.aspx
+static int os_info(void)
+{
+
+
+	return ERR_NONE;
+}
