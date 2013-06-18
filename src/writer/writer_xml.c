2e97c5179cfc32521266aaf15f528aea8adf379f
Andrew Benson <abenson@gmail.com>
2013-06-18 18:03:56 -0500

    Fix UNICODE support in XML writer.

--- src/writer/writer_xml.c
+++ src/writer/writer_xml.c
@@ -5,42 +5,42 @@
 
 int xml_begin(log_t *log)
 {
-	fprintf(log->file, "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<results>\n");
+	_ftprintf(log->file, _T("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<results>\n"));
 	return 0;
 }
 
 int xml_end(log_t *log)
 {
-	fprintf(log->file, "</results>\n");
+	_ftprintf(log->file, _T("</results>\n"));
 	return 0;
 }
 
 int xml_open_section(log_t *log, const _TCHAR *name)
 {
-	fprintf(log->file, "   <%s>\n", log->section);
+	_ftprintf(log->file, _T("   <%s>\n"), log->section);
 	return 0;
 }
 
 int xml_close_section(log_t *log)
 {
-	fprintf(log->file, "   </%s>\n", log->section);
+	_ftprintf(log->file, _T("   </%s>\n"), log->section);
 	return 0;
 }
 
 int xml_open_item(log_t *log)
 {
-	fprintf(log->file, "      <item>\n");
+	_ftprintf(log->file, _T("      <item>\n"));
 	return 0;
 }
 
 int xml_close_item(log_t *log)
 {
-	fprintf(log->file, "      </item>\n");
+	_ftprintf(log->file, _T("      </item>\n"));
 	return 0;
 }
 
 int xml_add_value(log_t *log, const _TCHAR *key, const _TCHAR *value)
 {
-	fprintf(log->file, "          <%s>%s</%s>\n", key, value, key);
+	_ftprintf(log->file, _T("          <%s>%s</%s>\n"), key, value, key);
 	return 0;
 }
