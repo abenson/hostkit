aab46af1a64598cca635277bfcdf58e19f830130
Andrew Benson <abenson@gmail.com>
2016-05-06 19:51:39 -0500

    Add 64-bit compile support.

--- src/common.mk
+++ src/common.mk
@@ -1,5 +1,8 @@
-CC=i686-w64-mingw32-gcc
-AR=i686-w64-mingw32-ar
+CC32=i686-w64-mingw32-gcc
+CC64=x86_64-w64-mingw32-gcc
+
+AR32=i686-w64-mingw32-ar
+AR64=x86_64-w64-mingw32-ar
 
 CFLAGS=
 LDFLAGS=-lnetapi32
