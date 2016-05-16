7e2112042c2b2239a23d0cabf8f22612a37b7111
Andrew Benson <abenson@gmail.com>
2016-05-16 12:15:54 -0500

    Enable unicode support.

--- src/common.mk
+++ src/common.mk
@@ -4,8 +4,8 @@ CC64=x86_64-w64-mingw32-gcc
 AR32=i686-w64-mingw32-ar
 AR64=x86_64-w64-mingw32-ar
 
-CFLAGS=
-LDFLAGS=-lnetapi32
+CFLAGS=-DUNICODE -D_UNICODE
+LDFLAGS=-lnetapi32 -municode
 
 ifdef X64
 	CC=$(CC64)
