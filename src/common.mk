6358c7e1809905010b0efb7ab3c06995f20529e3
Andrew Benson <abenson@gmail.com>
2016-05-16 13:23:11 -0500

    Correct UNICODE flags.

--- src/common.mk
+++ src/common.mk
@@ -4,8 +4,8 @@ CC64=x86_64-w64-mingw32-gcc
 AR32=i686-w64-mingw32-ar
 AR64=x86_64-w64-mingw32-ar
 
-CFLAGS=-DUNICODE -D_UNICODE
-LDFLAGS=-lnetapi32 -municode
+CFLAGS=-municode -DUNICODE -D_UNICODE
+LDFLAGS=-lnetapi32
 
 ifdef X64
 	CC=$(CC64)
