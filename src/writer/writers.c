9e917ada56c50a8550112de21d0388f2ef3e882c
Andrew Benson <abenson@gmail.com>
2016-05-16 23:01:32 -0500

    Enable all outputs.

--- src/writer/writers.c
+++ src/writer/writers.c
@@ -14,10 +14,8 @@
 struct wmodule modules[] =
 {
 	{_T("xml"), xml_begin, xml_end, xml_start_dict, xml_close_dict, xml_start_list, xml_close_list, xml_add_value},
-/*
 	{_T("json"), json_begin, json_end, json_start_dict, json_close_dict, json_start_list, json_close_list, json_add_value},
 	{_T("ruby"), ruby_begin, ruby_end, ruby_start_dict, ruby_close_dict, ruby_start_list, ruby_close_list, ruby_add_value},
-*/
 	{NULL, NULL, NULL, NULL, NULL, NULL}
 };
 
