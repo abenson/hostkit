187a864b92adeaa9e86ed823418fee35c38c9e2c
Andrew Benson <abenson@gmail.com>
2018-08-09 06:54:10 -0500

    makefile: allow choice of 32bit or 64bit build

--- src/common.mk
+++ src/common.mk
@@ -1,5 +1,11 @@
-CC=i686-w64-mingw32-gcc
-AR=i686-w64-mingw32-ar
+
+ifeq ($(WIN32),yes)
+	CC=i686-w64-mingw32-gcc
+	AR=i686-w64-mingw32-ar
+else
+	CC=x86_64-w64-mingw32-gcc
+	AR=x86_64-w64-mingw32-ar
+endif
 
 CFLAGS= 
 LDFLAGS=
