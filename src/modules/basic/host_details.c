161f3b68496a295453c63c9880a0e95c57fdb2f5
Andrew Benson <abenson@gmail.com>
2018-08-09 10:02:13 -0500

    modules/basic/host: add hostname

--- src/modules/basic/host_details.c
+++ src/modules/basic/host_details.c
@@ -1,8 +1,15 @@
+#include "../../writer/writer.h"
 #include "../../common.h"
+
+static int hostname(void);
 
 int host_details(void)
 {
+	open_section(scanLog, _T("host"));
 
+	hostname();
+
+	close_section(scanLog);
 	return 0;
 }
 
@@ -18,7 +25,9 @@ static int hostname(void)
 	name = malloc(sizeof(*name) * nameLen);
 	GetComputerNameExW(ComputerNameDnsHostname, name, &nameLen);
 
-	
+	open_item(scanLog);
+	add_value(scanLog, _T("hostname"), name);
+	close_item(scanLog);
 
 	free(name);
 }
