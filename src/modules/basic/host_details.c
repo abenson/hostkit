730b193bd1e03cc7f1c5aba6612e0f3dcfa99da8
Andrew Benson <abenson@gmail.com>
2019-04-02 13:58:25 -0500

    basic: add os name, version info

--- src/modules/basic/host_details.c
+++ src/modules/basic/host_details.c
@@ -1,8 +1,10 @@
 #include "../../writer/writer.h"
 #include "../../common.h"
 
+
 static int hostname(void);
 static int domainname(void);
+static int currentversion(void);
 
 int host_details(void)
 {
@@ -10,6 +12,7 @@ int host_details(void)
 
 	hostname();
 	domainname();
+	currentversion();
 
 	close_section(scanLog);
 	return 0;
@@ -19,6 +22,7 @@ static int hostname(void)
 {
 	_TCHAR *name = NULL;
 	DWORD nameLen = 0;
+	LONG status;
 
 	/* Get size of buffer. */
 	GetComputerNameEx(ComputerNamePhysicalDnsHostname, name, &nameLen);
@@ -30,6 +34,8 @@ static int hostname(void)
 	add_value(scanLog, _T("hostname"), name);
 
 	free(name);
+
+	return 0;
 }
 
 static int domainname(void)
@@ -47,4 +53,40 @@ static int domainname(void)
 	add_value(scanLog, _T("domainname"), name);
 
 	free(name);
+
+	return 0;
+}
+
+static int currentversion(void)
+{
+	_TCHAR *REG_CURRENTVERSION = _T("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+	HKEY key;
+	_TCHAR value[256];
+	DWORD keyType = REG_SZ;
+	DWORD valueLen;
+
+	RegOpenKeyEx(HKEY_LOCAL_MACHINE, REG_CURRENTVERSION, 0L, KEY_ALL_ACCESS, &key);
+
+	/* Product name */
+	valueLen = 256;
+	RegQueryValueEx(key, _T("ProductName"), NULL, &keyType, (LPBYTE)&value, &valueLen);
+	add_value(scanLog, _T("product"), value);
+
+	/* Current version */
+	valueLen = 256;
+	RegQueryValueEx(key, _T("CurrentVersion"), NULL, &keyType, (LPBYTE)&value, &valueLen);
+	add_value(scanLog, _T("version"), value);
+
+	/* Service pack */
+	valueLen = 256;
+	RegQueryValueEx(key, _T("CSDVersion"), NULL, &keyType, (LPBYTE)&value, &valueLen);
+	add_value(scanLog, _T("splevel"), value);
+
+	/* Build number */
+	valueLen = 256;
+	RegQueryValueEx(key, _T("CurrentBuildNumber"), NULL, &keyType, (LPBYTE)&value, &valueLen);
+	add_value(scanLog, _T("build"), value);
+
+	RegCloseKey(key);
+	return 0;
 }
