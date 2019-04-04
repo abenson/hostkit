e263e8389c0b8795ae08a5dbf180f20e52fc4115
Andrew Benson <abenson@gmail.com>
2019-04-04 10:17:48 -0500

    drives: enumerate mounted drives

--- src/modules/basic/drives.c
+++ src/modules/basic/drives.c
@@ -6,24 +6,51 @@
 
 int enum_drives(void)
 {
-	TCHAR guid[BUFLEN];
-	HANDLE handle;
+	TCHAR drives[BUFLEN];
+	int i = 0;
+	DWORD type;
 
-	handle = FindFirstVolume(guid, BUFLEN);
+	GetLogicalDriveStrings(BUFLEN, drives);
 
 	start_itemlist(scanLog, _T("drives"));
 
-	start_itemlist_item(scanLog);
-	add_value(scanLog, _T("guid"), guid);
-	end_itemlist_item(scanLog);
-
-	while(FindNextVolume(handle, guid, BUFLEN)) {
+	while(drives[i]) {
 		start_itemlist_item(scanLog);
-		add_value(scanLog, _T("guid"), guid);
+
+		add_value(scanLog, _T("id"), drives+i);
+
+		type = GetDriveType(drives+i);
+
+		switch(type) {
+			case DRIVE_NO_ROOT_DIR:
+				add_value(scanLog, _T("type"), _T("invalid"));
+				break;
+			case DRIVE_REMOVABLE:
+				add_value(scanLog, _T("type"), _T("removable"));
+				break;
+			case DRIVE_FIXED:
+				add_value(scanLog, _T("type"), _T("fixed"));
+				break;
+			case DRIVE_REMOTE:
+				add_value(scanLog, _T("type"), _T("remote"));
+				break;
+			case DRIVE_CDROM:
+				add_value(scanLog, _T("type"), _T("cdrom"));
+				break;
+			case DRIVE_RAMDISK:
+				add_value(scanLog, _T("type"), _T("ramdisk"));
+				break;
+			case DRIVE_UNKNOWN:
+			default:
+				add_value(scanLog, _T("type"), _T("unknown"));
+				break;
+		}
+
 		end_itemlist_item(scanLog);
+
+		i += _tcslen(drives+i) + 1;
 	}
-
 	end_itemlist(scanLog);
-	FindVolumeClose(handle);
 
+	return 0;
 }
