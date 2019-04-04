0f27cecc31ff679bae572a9928dc3ce9b8915772
Andrew Benson <abenson@gmail.com>
2019-04-04 13:06:03 -0500

    drives: we do want guid, mount point, and type

--- src/modules/basic/drives.c
+++ src/modules/basic/drives.c
@@ -2,54 +2,56 @@
 #include "../../common.h"
 
 #undef BUFLEN
-#define BUFLEN 256
+#define BUFLEN 32768
 
 int enum_drives(void)
 {
-	TCHAR drives[BUFLEN];
-	int i = 0;
-	DWORD type;
-
-	GetLogicalDriveStrings(BUFLEN, drives);
+	TCHAR guid[BUFLEN], name[BUFLEN];
+	HANDLE handle;
+	unsigned long i = 0, c, type;
 
 	start_itemlist(scanLog, _T("drives"));
 
-	while(drives[i]) {
+	handle = FindFirstVolume(guid, BUFLEN);
+
+	do {
 		start_itemlist_item(scanLog);
 
-		add_value(scanLog, _T("id"), drives+i);
+		add_value(scanLog, _T("guid"), guid);
 
-		type = GetDriveType(drives+i);
-
-		switch(type) {
-			case DRIVE_NO_ROOT_DIR:
-				add_value(scanLog, _T("type"), _T("invalid"));
-				break;
-			case DRIVE_REMOVABLE:
-				add_value(scanLog, _T("type"), _T("removable"));
-				break;
-			case DRIVE_FIXED:
-				add_value(scanLog, _T("type"), _T("fixed"));
-				break;
-			case DRIVE_REMOTE:
-				add_value(scanLog, _T("type"), _T("remote"));
-				break;
-			case DRIVE_CDROM:
-				add_value(scanLog, _T("type"), _T("cdrom"));
-				break;
-			case DRIVE_RAMDISK:
-				add_value(scanLog, _T("type"), _T("ramdisk"));
-				break;
-			case DRIVE_UNKNOWN:
-			default:
-				add_value(scanLog, _T("type"), _T("unknown"));
-				break;
+		if(GetVolumePathNamesForVolumeName(guid, name, BUFLEN, &c) != 0) {
+			if(name[0]) {
+				add_value(scanLog, _T("volume"), name);
+				type = GetDriveType(name);
+				switch(type) {
+					case DRIVE_NO_ROOT_DIR:
+						add_value(scanLog, _T("type"), _T("invalid"));
+						break;
+					case DRIVE_REMOVABLE:
+						add_value(scanLog, _T("type"), _T("removable"));
+						break;
+					case DRIVE_FIXED:
+						add_value(scanLog, _T("type"), _T("fixed"));
+						break;
+					case DRIVE_REMOTE:
+						add_value(scanLog, _T("type"), _T("remote"));
+						break;
+					case DRIVE_CDROM:
+						add_value(scanLog, _T("type"), _T("cdrom"));
+						break;
+					case DRIVE_RAMDISK:
+						add_value(scanLog, _T("type"), _T("ramdisk"));
+						break;
+					case DRIVE_UNKNOWN:
+					default:
+						add_value(scanLog, _T("type"), _T("unknown"));
+						break;
+				}
+			}
 		}
-
 		end_itemlist_item(scanLog);
-
-		i += _tcslen(drives+i) + 1;
-	}
+	} while (FindNextVolume(handle, guid, BUFLEN));
+	
 	end_itemlist(scanLog);
 
 	return 0;
